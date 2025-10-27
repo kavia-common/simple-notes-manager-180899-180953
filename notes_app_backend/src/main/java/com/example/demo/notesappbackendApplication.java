c5ad0d2225f5f2fdf326f781f2dd107e25a412f1
Kavia AI <production@kavia.ai>
2025-10-27 12:56:48 +0000

    CheckPoint - cg535f5002

---
+++ notes_app_backend/src/main/java/com/example/demo/notesappbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.notesappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class notesappbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(notesappbackendApplication.class, args);
+	}
+
+}
